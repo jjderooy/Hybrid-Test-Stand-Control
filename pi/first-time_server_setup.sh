dd1795e5bfdde978a9855f9bae6e2b70b4b2c886
jjderooy <jj.derooy123@gmail.com>
2020-05-19 13:54:24 -0700

    Added automatic installer code

--- pi/first-time_server_setup.sh
+++ pi/first-time_server_setup.sh
@@ -1,1 +1,17 @@
+# This aims script automates the setup of the Raspberry Pi server. It automagically installs
+# python modules, packages, creates necessary folders, etc.
 
+# Change directory (cd) to the home directory
+cd ~/
+
+# Make a folder (mkdir) called Hybrid_Server there
+mkdir Hybrid_Server
+
+# Navigate into that folder
+cd Hybrid_Server
+
+# Download (wget) necessary files from UVic Rocketry GitHub
+wget https://github.com/UVicRocketry/Hybrid-Test-Stand-Control/blob/master/pi/starthtsc.sh
+
+# Install (pip3) python3 module  phidget22 for stepper motor controller boards
+pip3 install phidget22
