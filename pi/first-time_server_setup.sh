cd9a1edbb435c9fa0b79c53541831b6040fc7928
JJ De Rooy <59892080+jjderooy@users.noreply.github.com>
2020-05-19 17:35:14 -0700

    Update first-time_server_setup.sh

--- pi/first-time_server_setup.sh
+++ pi/first-time_server_setup.sh
@@ -15,9 +15,13 @@ pip3 install phidget22
 # 777 allows anyone to read an write to the file. 
 chmod 777 /etc/rc.local
 
-# Edit (sed) rc.local to automatically start the server when the Pi turns on. /exit 0/i inserts before "exit 0"
+# Edit (sed) rc.local to automatically start the server when the Pi turns on. /exit 0/i inserts before "exit 0".
+# temp is a string that stores the edited contents of rc.local. temp is then written (>) back to rc.local.
+# Done this way to prevent issues of reading a writing to the same file at the same time.
 	# From the rc.local file on the GitHub that this line replaces:
 	# Put this line before the exit statement in /etc/rc.local.
 	# The 30 second sleep is to allow the pi's networking to
 	# initialize (the 30s is arbitrary but seems to work).
-sed  '/exit 0/i (sleep 30s && /home/pi/Documents/htsc/pi/starthtsc.sh) &' /etc/rc.local
+temp=$(sed '/exit 0/i (sleep 30s && /home/pi/Documents/htsc/pi/starthtsc.sh) &' /etc/rc.local)
+echo "$temp" > /etc/rc.local
+
