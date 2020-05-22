8dc5e7be5606f8589186ac11786f37274dcc3a82
JJ De Rooy <59892080+jjderooy@users.noreply.github.com>
2020-05-22 16:33:40 -0700

    Update starthtsc.sh
    Delay for networking is now in here due to switch to cron from rc.local. cron runs immediately after boot and calls this script so delay is in this file now.

--- server/starthtsc.sh
+++ server/starthtsc.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+# Wait 30 seconds to make sure networking is running.
 # f is a string that stores the next log file while the server is running.
 # && chains two commands together.
 # Then use python3 to run testmain.py and start adding any console output to f.
 # Lastly if either of the first two parts fail, print a failure message.
+sleep 30
 f=$(/home/pi/Documents/htsc/server/nextlog.py /home/pi/Documents/logs) && /usr/bin/python3 /home/pi/Documents/htsc/server/testmain.py > $f 2>&1 || echo "failed to start HTSC server"
